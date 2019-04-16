4832f76a0a728e8a0706461e778e0d33b1ea66ce
adamP2 <adampolejczuk@gmail.com>
2019-04-16 15:47:51 +0200

    edycja xmla2

--- adam/project/src/java/Servlet1.java
+++ adam/project/src/java/Servlet1.java
@@ -7,8 +7,11 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +20,9 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -42,21 +48,65 @@ public class Servlet1 extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            try {
-                File xmlFile = new File("\"C:\\Users\\aniap\\Desktop\\projekt www\\ProjectWWW\\adam\\project\\src\\java\\wordsList.xml\""); 
-                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder db;
-                db = dbFactory.newDocumentBuilder();
-                Document document = db.parse(xmlFile);
-            document.getDocumentElement().normalize();
-            } catch (ParserConfigurationException ex) {
-                Logger.getLogger(Servlet1.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (SAXException ex) {
-                Logger.getLogger(Servlet1.class.getName()).log(Level.SEVERE, null, ex);
-            }
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Servlet1 at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Words: </h1></br>");
+            ServletContext sc = getServletContext();
+            try {
+                    File fileXML = new File("C:\\Users\\aniap\\Desktop\\projekt www\\ProjectWWW\\adam\\project\\src\\java\\wordsList2.xml");
+
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(fileXML);
+
+                    doc.getDocumentElement().normalize();
+
+                    NodeList nList = doc.getElementsByTagName("word");
+
+                    List<String> pol = new ArrayList<String>();
+                    List<String> eng = new ArrayList<String>();
+                    for (int i = 0; i < nList.getLength(); i++) {
+                        Node data = nList.item(i);
+
+                        if (data.getNodeType() == Node.ELEMENT_NODE) {
+                            Element element = (Element) data;
+
+                            pol.add(element.getElementsByTagName("item").item(0).getTextContent());
+                            out.println("PL: ");
+                            out.println(element.getElementsByTagName("item").item(0).getTextContent()+"</br>");
+                            eng.add(element.getElementsByTagName("item").item(1).getTextContent());
+                            out.println("ENG: ");
+                            out.println(element.getElementsByTagName("item").item(1).getTextContent());
+                            out.println("<form method=\"post\" action=\"DeleteServlet\">");
+                            out.println("<input type=\"hidden\" name =\"id\" value=\""+i+"\"/>");
+                            out.println("<input type=\"submit\" value=\"delete\"/></br>");
+                            out.println("</form>");
+                            out.println("<form method=\"post\" action=\"EditWordServlet\">");
+                            out.println("<input type=\"hidden\" name =\"id\" value=\""+i+"\"/>");
+                            out.println("<input type=\"submit\" value=\"edit\"/></br>");
+                            out.println("</form>");
+                            out.println("</br></br>");
+                        }
+                    }
+
+                    sc.setAttribute("pol", pol);
+                    sc.setAttribute("eng", eng);
+
+            out.println("<form method=\"post\" action=\"AddServlet\">");
+            out.println("</br>Add Word: ");
+            out.println("<input type=\"text\" name=\"namePL\"/>");
+            out.println("<input type=\"text\" name=\"nameENG\"/>");
+            out.println("<input type=\"submit\" value=\"add\"/></br>");
+            out.println("</br>Na liscie w parametrach: </br>");
+            for(int i=0; i<pol.size();i++){
+                out.println("PL: "+pol.get(i)+"</br>");
+                out.println("ENG: "+eng.get(i)+"</br></br>");
+            }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
