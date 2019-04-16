7d0f3ba572c78be508b54649a7e2b2e90d6c2d89
Mateusz Samluk <mazsak97@gmail.com>
2019-04-16 13:36:58 +0200

    add read xml

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,64 +1,61 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
-import javax.servlet.http.HttpSessionBindingEvent;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Listener implements ServletContextListener,
-        HttpSessionListener, HttpSessionAttributeListener {
+public class Listener implements ServletContextListener{
 
-    // Public constructor is required by servlet spec
-    public Listener() {
-    }
-
-    // -------------------------------------------------------
-    // ServletContextListener implementation
-    // -------------------------------------------------------
     public void contextInitialized(ServletContextEvent sce) {
-      /* This method is called when the servlet context is
-         initialized(when the Web application is deployed). 
-         You can initialize servlet context related data here.
-      */
+        ServletContext sc = sce.getServletContext();
+        synchronized (sce.getServletContext()) {
+            String nameFile = sc.getInitParameter("nameFile");
+
+            try {
+                    File fileXML = new File("D:\\Projects\\Java\\WWW\\ProjectWWW\\" + nameFile);
+
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(fileXML);
+
+                    doc.getDocumentElement().normalize();
+
+                    NodeList nList = doc.getElementsByTagName("word");
+
+                    List<String> pol = new ArrayList<String>();
+                    List<String> eng = new ArrayList<String>();
+                    for (int i = 0; i < nList.getLength(); i++) {
+                        Node data = nList.item(i);
+
+                        if (data.getNodeType() == Node.ELEMENT_NODE) {
+                            Element element = (Element) data;
+
+                            pol.add(element.getElementsByTagName("item").item(0).getTextContent());
+                            eng.add(element.getElementsByTagName("item").item(1).getTextContent());
+                        }
+                    }
+
+                    sc.setAttribute("pol", pol);
+                    sc.setAttribute("eng", eng);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-      /* This method is invoked when the Servlet Context 
-         (the Web application) is undeployed or 
-         Application Server shuts down.
-      */
-    }
 
-    // -------------------------------------------------------
-    // HttpSessionListener implementation
-    // -------------------------------------------------------
-    public void sessionCreated(HttpSessionEvent se) {
-        /* Session is created. */
-    }
-
-    public void sessionDestroyed(HttpSessionEvent se) {
-        /* Session is destroyed. */
-    }
-
-    // -------------------------------------------------------
-    // HttpSessionAttributeListener implementation
-    // -------------------------------------------------------
-
-    public void attributeAdded(HttpSessionBindingEvent sbe) {
-      /* This method is called when an attribute 
-         is added to a session.
-      */
-    }
-
-    public void attributeRemoved(HttpSessionBindingEvent sbe) {
-      /* This method is called when an attribute
-         is removed from a session.
-      */
-    }
-
-    public void attributeReplaced(HttpSessionBindingEvent sbe) {
-      /* This method is invoked when an attibute
-         is replaced in a session.
-      */
     }
 }
